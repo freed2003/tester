5e05028938e78d2885854fb7be3cfa2dd2e66021
Gary Song <gary.song2003@gmail.com>
2023-09-14 08:19:26 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import argparse
+from model import model
+def main():
+    pass
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    main()
---
+++ model.py
@@ -0,0 +1,9 @@
+"""
+    Place model here
+    Args:
+        -orig_data: origintal time-series data
+    Output:
+        Generated data
+"""
+def model(orig_data):
+    pass
